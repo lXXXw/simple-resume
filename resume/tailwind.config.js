793ae88dfbbfd72a40c56cf35ff2bb77d7a3b855
lxxxw <lxxxw.vido@gmail.com>
2023-11-04 22:41:42 -0700

    dummy color change

--- resume/tailwind.config.js
+++ resume/tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "accent-color": "#FA3121 ",
+        "accent-color": "#22C55E",
       },
     },
     fontSize: {
